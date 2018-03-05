6165546c1a15217daa7467235a9b25c0b6db88aa
medusade <medusade@cox.net>
2018-03-05 08:34:25 -0800

    Updated thirdparty source.

---
+++ thirdparty/gnu/mp-0.0.0/src/gnu/app/console/mp/main_main.hpp
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////
+/// Copyright (c) 1988-2018 $organization$
+///
+/// This software is provided by the author and contributors ``as is'' 
+/// and any express or implied warranties, including, but not limited to, 
+/// the implied warranties of merchantability and fitness for a particular 
+/// purpose are disclaimed. In no event shall the author or contributors 
+/// be liable for any direct, indirect, incidental, special, exemplary, 
+/// or consequential damages (including, but not limited to, procurement 
+/// of substitute goods or services; loss of use, data, or profits; or 
+/// business interruption) however caused and on any theory of liability, 
+/// whether in contract, strict liability, or tort (including negligence 
+/// or otherwise) arising in any way out of the use of this software, 
+/// even if advised of the possibility of such damage.
+///
+///   File: main_main.hpp
+///
+/// Author: $author$
+///   Date: 3/5/2018
+///////////////////////////////////////////////////////////////////////
+#ifndef _GNU_APP_CONSOLE_MP_MAIN_MAIN_HPP
+#define _GNU_APP_CONSOLE_MP_MAIN_MAIN_HPP
+
+#include "mpz_msb.h"
+
+namespace gnu {
+namespace app {
+namespace console {
+namespace mp {
+
+} // namespace mp 
+} // namespace console 
+} // namespace app 
+} // namespace gnu 
+
+#endif // _GNU_APP_CONSOLE_MP_MAIN_MAIN_HPP 
